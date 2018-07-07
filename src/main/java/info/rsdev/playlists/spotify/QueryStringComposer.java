52c122a7c50fd5e385ebe787f3895e02c3413bcd
Dave Schoorl <daveloper.rex@gmail.com>
2018-07-07 19:34:54 +0200

    Really fix failing unittests

--- src/main/java/info/rsdev/playlists/spotify/QueryStringComposer.java
+++ src/main/java/info/rsdev/playlists/spotify/QueryStringComposer.java
@@ -28,76 +28,76 @@ import java.util.stream.Collectors;
 import info.rsdev.playlists.domain.Song;
 
 public class QueryStringComposer {
-	
-	private static final Set<String> ARTIST_NOISE_WORDS = new HashSet<>();
-	private static final Set<String> TITLE_NOISE_WORDS = new HashSet<>();
-	private static final Map<String, String> ARTIST_ALIASSES = new HashMap<>();
-	static {
-		//all entries must be lower case
-		ARTIST_NOISE_WORDS.addAll(Arrays.asList("feat", "feat.", "featuring", "ft.", "ft", "the", "with", "and", "x", "+", "vs", "vs."));
-		TITLE_NOISE_WORDS.addAll(Arrays.asList("the", "a", "de", "-", "radio", "edit", "mix", "single"));
-		
-		ARTIST_ALIASSES.put("atc", "a touch of class");
-		ARTIST_ALIASSES.put("beegees", "bee gees");
-	}
-	
-	public static Set<String> normalizeArtist(Song song) {
-        Set<String> artistWords = splitToLowercaseWords(song.artist);
+    
+    private static final Set<String> ARTIST_NOISE_WORDS = new HashSet<>();
+    private static final Set<String> TITLE_NOISE_WORDS = new HashSet<>();
+    private static final Map<String, String> ARTIST_ALIASSES = new HashMap<>();
+    static {
+        //all entries must be lower case
+        ARTIST_NOISE_WORDS.addAll(Arrays.asList("feat", "feat.", "featuring", "ft.", "ft", "the", "with", "and", "x", "+", "vs", "vs."));
+        TITLE_NOISE_WORDS.addAll(Arrays.asList("the", "a", "de", "-", "radio", "edit", "mix", "single"));
+        
+        ARTIST_ALIASSES.put("atc", "a touch of class");
+        ARTIST_ALIASSES.put("beegees", "bee gees");
+    }
+    
+    public static SortedSet<String> normalizeArtist(Song song) {
+        SortedSet<String> artistWords = splitToLowercaseWords(song.artist);
         artistWords.removeAll(ARTIST_NOISE_WORDS);
-        artistWords = artistWords.stream().map(QueryStringComposer::replaceAliasses).collect(Collectors.toSet());
+        artistWords = artistWords.stream().map(QueryStringComposer::replaceAliasses).collect(Collectors.toCollection(() -> new TreeSet<>()));
         return artistWords;
-	}
-	
-    public static Set<String> normalizeTitle(Song song) {
+    }
+    
+    public static SortedSet<String> normalizeTitle(Song song) {
         String title = chooseOneWhenThereIsDoubleASide(song.title);
-        Set<String> titleWords = splitToLowercaseWords(title);
+        SortedSet<String> titleWords = splitToLowercaseWords(title);
         titleWords.removeAll(TITLE_NOISE_WORDS);
         return titleWords;
     }
     
-	public static String makeQueryString(Song song) throws UnsupportedEncodingException {
-	    Set<String> titleWords = normalizeTitle(song);
-	    Set<String> artistWords = normalizeArtist(song);
-		StringBuilder query = new StringBuilder();
-		appendSearchField(query, "artist", artistWords);
-		if (!artistWords.isEmpty()) {
-			query.append(" ");
-		}
-		appendSearchField(query, "title", titleWords);
-		return query.toString();
-	}
-	
-	private static String chooseOneWhenThereIsDoubleASide(String title) {
-		if (title.contains("/")) {
-			return title.substring(0, title.indexOf("/"));
-		}
-		return title;
-	}
+    public static String makeQueryString(Song song) throws UnsupportedEncodingException {
+        SortedSet<String> titleWords = normalizeTitle(song);
+        SortedSet<String> artistWords = normalizeArtist(song);
+        StringBuilder query = new StringBuilder();
+        appendSearchField(query, "artist", artistWords);
+        if (!artistWords.isEmpty()) {
+            query.append(" ");
+        }
+        appendSearchField(query, "title", titleWords);
+        return query.toString();
+    }
+    
+    private static String chooseOneWhenThereIsDoubleASide(String title) {
+        if (title.contains("/")) {
+            return title.substring(0, title.indexOf("/"));
+        }
+        return title;
+    }
 
-	private static String replaceAliasses(String word) {
-		if (ARTIST_ALIASSES.containsKey(word)) {
-			return ARTIST_ALIASSES.get(word);
-		}
-		return word;
-	}
-	
-	private static StringBuilder appendSearchField(StringBuilder query, String fieldName, Set<String> words) throws UnsupportedEncodingException {
-		if (!words.isEmpty()) {
-			query.append(fieldName).append(":");
-			query.append(words.stream().collect(Collectors.joining(" ")));
-		}
-		return query;
-	}
+    private static String replaceAliasses(String word) {
+        if (ARTIST_ALIASSES.containsKey(word)) {
+            return ARTIST_ALIASSES.get(word);
+        }
+        return word;
+    }
+    
+    private static StringBuilder appendSearchField(StringBuilder query, String fieldName, SortedSet<String> words) throws UnsupportedEncodingException {
+        if (!words.isEmpty()) {
+            query.append(fieldName).append(":");
+            query.append(words.stream().collect(Collectors.joining(" ")));
+        }
+        return query;
+    }
 
-	private static SortedSet<String> splitToLowercaseWords(String field) {
-		String[] words = field.toLowerCase().split("[\\s\\(\\)\\,\\&]");
-		SortedSet<String> result = new TreeSet<>();	//apply alphabetic ordering to make unit testing easier
-		for (String word : words) {
-			if ((word != null) && !word.isEmpty()) {
-				result.add(word);
-			}
-		}
-		return result;
-	}
-	
+    private static SortedSet<String> splitToLowercaseWords(String field) {
+        String[] words = field.toLowerCase().split("[\\s\\(\\)\\,\\&]");
+        SortedSet<String> result = new TreeSet<>(); //apply alphabetic ordering to make unit testing easier
+        for (String word : words) {
+            if ((word != null) && !word.isEmpty()) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+    
 }
