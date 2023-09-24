4d71bb66209ea9b74bbe095e3b3de6986f598d33
Dave Schoorl <dave.schoorl@kb.nl>
2023-09-24 23:40:11 +0200

    Prepare skeleton web GUI set up with yarn as packagemanager

--- src/main/java/info/rsdev/playlists/dao/ChartsItemSqlConstants.java
+++ src/main/java/info/rsdev/playlists/dao/ChartsItemSqlConstants.java
@@ -20,8 +20,8 @@ public final class ChartsItemSqlConstants {
 	public static final String ARTIST_COLUMN = "artist";
 	public static final String TITLE_COLUMN = "title";
 	
-	public static final String GET_NEW_RELEASES_QUERY = String.format("select %s, %s from %s where %s = ? group by %s, %s",
-	        ARTIST_COLUMN, TITLE_COLUMN, CHART_ITEM_TABLE, YEAR_COLUMN, ARTIST_COLUMN, TITLE_COLUMN);
+	public static final String GET_NEW_RELEASES_QUERY = String.format("select %s, %s from %s where %s = ? and %s = true group by %s, %s",
+	        ARTIST_COLUMN, TITLE_COLUMN, CHART_ITEM_TABLE, YEAR_COLUMN, IS_NEW_IN_CHART, ARTIST_COLUMN, TITLE_COLUMN);
 	
 	public static final String GET_HIGHEST_YEAR = String.format("select max(%s) from %s where %s = ?", 
 	        YEAR_COLUMN, CHART_ITEM_TABLE, CHART_NAME_COLUMN);
