a2bb80c8284842bc30b4ba44faa425c566d81399
Dave Schoorl <1429321+dschoorl@users.noreply.github.com>
2024-08-19 10:20:33 +0200

    Update database with charts data on server startup

--- playlist-server/src/main/java/info/rsdev/playlists/Playlists.java
+++ playlist-server/src/main/java/info/rsdev/playlists/Playlists.java
@@ -15,49 +15,41 @@
  */
 package info.rsdev.playlists;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import info.rsdev.playlists.services.MusicTitleService;
+import jakarta.annotation.Resource;
 
 /**
  * @author Dave Schoorl
  */
 @SpringBootApplication
-public class Playlists {
+public class Playlists  implements CommandLineRunner {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(Playlists.class);
-//    private static final String PLAYLIST_NAME_TEMPLATE = "%d charted songs";
+    private static final Logger LOGGER = LoggerFactory.getLogger(Playlists.class);
     
-//    @Resource
-//    private MusicTitleService titleService;
-//
-//    @Resource
-//    private PlaylistService playlistService;
+    @Resource
+    private MusicTitleService titleService;
 
     public static void main(String[] args) {
         SpringApplication.run(Playlists.class, args);
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//        var startTime = System.currentTimeMillis();
-//
-//        if (LOGGER.isInfoEnabled()) {
-//            LOGGER.info("Program arguments: {}", Arrays.toString(args));
-//        }
-//        var year = getYear(args);
-//
-//        titleService.init();
-//        LOGGER.info("Datastore initialized after {}s", (System.currentTimeMillis() - startTime) / 1000);
-//
-//        var chartedSongs = titleService.getChartedSongsForYear(year);
-//        playlistService.fillPlaylistWithSongs(String.format(PLAYLIST_NAME_TEMPLATE, year), chartedSongs);
-//
-//        LOGGER.info("Finished: {}s", (System.currentTimeMillis() - startTime) / 1000);
-//    }
-//
-//    private short getYear(String[] args) {
-//        // too little time, implemented quick and dirty -- no validation or error
-//        // checking, expect the last program argument to be the year
-//        return args.length > 0 ? Short.parseShort(args[args.length - 1]) : (short) LocalDate.now().getYear();
-//    }
+    @Override
+    public void run(String... args) throws Exception {
+        var startTime = System.currentTimeMillis();
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Program arguments: {}", Arrays.toString(args));
+        }
+
+        titleService.init();
+        LOGGER.info("Datastore initialized after {}s", (System.currentTimeMillis() - startTime) / 1000);
+    }
 }
